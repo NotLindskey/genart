f2a1f6457e1a60e1bfc44f6ff5778d1856c7e9bc
Linssi Colin Ruiz <linssi101@gmail.com>
2023-07-10 20:27:02 -0500

    added colors at random

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
+const colors = ['red', 'yellow', 'green', 'cyan', 'blue', 'purple', 'orange'];
+
+function randColor() {
+  let index = Math.floor(Math.random() * colors.length);
+
+  return colors[index];
+}
+
 function setup() {
   createCanvas(600, 600);
 }
+
+let bgColor = randColor();
+let bodyColor = randColor();
+let earsColor = randColor();
+let innerEarsColor = randColor();
+let eyeColor = randColor();
+let faceColor = randColor();
+let snoutColor = randColor();
 
 function draw() {
   strokeWeight(5);
 
   //background
-  background('red');
+  background(bgColor);
 
   //body
-  fill('yellow');
+  fill(bodyColor);
   ellipse(300, 690, 640, 440);
 
   // ears
-  fill('green');
+  fill(earsColor);
   ellipse(125, 135, 170, 170);
   ellipse(475, 135, 170, 170);
 
   // inner ears
-  fill('cyan');
+  fill(innerEarsColor);
   ellipse(125, 135, 140, 140);
   ellipse(475, 135, 140, 140);
 
   // face
-  fill('blue');
+  fill(faceColor);
   ellipse(300, 330, 450, 440);
 
   //eyes
-  fill('purple');
+  fill(eyeColor);
   ellipse(230, 250, 82, 82);
   ellipse(370, 250, 82, 82);
 
@@ -37,7 +53,7 @@ function draw() {
   ellipse(370, 250, 30, 30);
 
   // snout
-  fill('orange');
+  fill(snoutColor);
   ellipse(300, 390, 200, 200);
 
   // mouth
